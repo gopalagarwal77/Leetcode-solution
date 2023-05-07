8f6b5fc08ba25ded7ddb8c7020e4705c30ff982a
gopalagarwal77 <93563296+gopalagarwal77@users.noreply.github.com>
2023-05-08 00:20:48 +0530

    Added solution - LeetHub

---
+++ Return two prime numbers - GFG/return-two-prime-numbers.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            
+            Solution ob = new Solution();
+            List<Integer> ans = new ArrayList<Integer>();
+            ans = ob.primeDivision(N);
+            System.out.println(ans.get(0) + " " + ans.get(1));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public static boolean  isprime(int n){
+        if(n==2 || n==3){
+            return true;
+        }
+        if(n%2==0 || n%3==0){
+            return false;
+        }
+        for(int i=5;i*i<=n;i+=6){
+            if(n%i==0 || n%(i+2)==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    static List<Integer> primeDivision(int N){
+        // code here
+        ArrayList<Integer> arr =new ArrayList<>();
+        for(int i=2;i<N;i++){
+            if(isprime(i) && isprime(N-i)){
+                arr.add(i);
+                arr.add(N-i);
+                return arr;
+            }
+        }
+        return arr;
+    }
+}
